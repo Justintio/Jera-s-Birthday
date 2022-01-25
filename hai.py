b32b986407bea053f6ae6744d6a07e2c15847e74
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-25 20:50:33 +0700

    Update hai.py

--- hai.py
+++ hai.py
@@ -2,12 +2,20 @@ import streamlit as st
 from PIL import Image
 
 st.title('Hai.')
-st.write('Happy Birthday Cantikku')
 
-left_col, mid_col, right_col = st.columns(3)
-image1 = Image.open('jera1.jpeg')
-left_col.image(image1)
-image2 = Image.open('jera2.jpeg')
-mid_col.image(image2)
-image3 = Image.open('jera3.jpeg')
-right_col.image(image3)
+bener = ['kosong', 'bener', 'salah']
+nama = st.selectbox('bener ga hari ini ulang tahun ? ', bener)
+
+if bener == 'salah' :
+  st.write('boong lu !')
+  
+if bener == 'bener' :
+  st.write('Happy Birthday Cantikku')
+
+  left_col, mid_col, right_col = st.columns(3)
+  image1 = Image.open('jera1.jpeg')
+  left_col.image(image1)
+  image2 = Image.open('jera2.jpeg')
+  mid_col.image(image2)
+  image3 = Image.open('jera3.jpeg')
+  right_col.image(image3)
