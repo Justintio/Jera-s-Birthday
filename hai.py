79fc6b0f585c652c86ea6e841a3962bf448d632e
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-25 20:35:46 +0700

    Update hai.py

--- hai.py
+++ hai.py
@@ -5,5 +5,5 @@ st.title('Hai.')
 st.write('Happy Birthday Cantikku')
 
 left_col, mid_col, right_col = st.columns(3)
-image1 = Image.open('jera1.jpg')
+image1 = Image.open('jera1.jpeg')
 left_col.image(image1)
