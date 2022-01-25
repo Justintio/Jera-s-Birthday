e4bdc3f0b06c3524b634230f9d9271745001b9b7
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-25 20:33:17 +0700

    Update hai.py

--- hai.py
+++ hai.py
@@ -2,4 +2,8 @@ import streamlit as st
 from PIL import Image
 
 st.title('Hai.')
-st.write('Happy Birthday Cantik')
+st.write('Happy Birthday Cantikku')
+
+left_col, mid_col, right_col = st.columns(3)
+image1 = Image.open('jera1.jpg')
+left_col.image(image1)
