a0c5727a6929e68276489804aeea5852459549ad
Justintio <97281500+Justintio@users.noreply.github.com>
2022-01-25 21:06:34 +0700

    Update hai.py

--- hai.py
+++ hai.py
@@ -12,11 +12,18 @@ if nama == 'salah' :
 if nama == 'bener' :
   st.title('Happy Birthday Cantik')
   st.write('May all of your wishes will come true')
-
+  
+  st.title('  ')
   left_col, mid_col, right_col = st.columns(3)
   image1 = Image.open('jera1.jpeg')
   left_col.image(image1)
+  st.title('  ')  
   image2 = Image.open('jera2.jpeg')
   mid_col.image(image2)
+  st.title('  ')
   image3 = Image.open('jera3.jpeg')
   right_col.image(image3)
+  st.title('  ')
+  st.title('Love you')
+  st.write('Udah yak gitu aja, gabisa jadi manis ')
+  st.write('bisanya mencintaimu ahay ')
